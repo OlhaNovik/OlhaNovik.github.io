fe7db4bd836f0654c7bcecd5b042aa50217d528c
OlyaNovik <olyaanovikk@gmail.com>
2024-05-20 19:14:05 +0300

    add animation in main page

--- animation-app/src/Component/PagesGloba/Footer.jsx
+++ animation-app/src/Component/PagesGloba/Footer.jsx
@@ -1,16 +1,44 @@
 import "../../Style/StyleGobal/Footer.scss"
 import imgFooter from "../../img/footer_img.png"
+import React, { useEffect, useRef } from 'react';
+import anime from 'animejs/lib/anime.es.js';
+
 
 const Footer = () => {
+  const imageRef = useRef(null);
 
+  const handleMouseEnter = () => {
+    anime({
+      targets: imageRef.current,
+    //   translateX: 250,
+      rotate: '1turn',
+      duration: 2000,
+      easing: 'easeInOutQuart'
+    });
+  };
 
-    return (
-        <>
-        <div className="footer_box">
-           <img src={imgFooter} alt="" />
-        </div>
-            </>
-    )
+  const handleMouseLeave = () => {
+    anime({
+      targets: imageRef.current,
+      translateX: 0,
+      rotate: '0turn',
+      duration: 2000,
+      easing: 'easeInOutQuart'
+    });
+  };
 
-}
+  return (
+    <div className="footer_box">
+    <img
+      ref={imageRef}
+      src={imgFooter}
+      alt="Animated"
+      style={{ width: '970px', height: '960px', transition: 'transform 0.2s' }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    />
+    </div>
+  );
+};
+
 export default Footer;
